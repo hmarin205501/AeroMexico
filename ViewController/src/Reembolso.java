72176c0a675eba4ca864ac6f82fc14441c4109d1
hmarin205501 <hmarin@multisolutions.com.mx>
2017-02-23 13:41:34 -0600

    Cadena de costo total por tickets seleccionados, se imprime en un pop-up

--- ViewController/src/Reembolso.java
+++ ViewController/src/Reembolso.java
@@ -8,17 +8,19 @@ import oracle.jbo.Row;
 import oracle.jbo.ViewObject;
 
 public class Reembolso {
+    private String parametroReembolso;
+    
     public Reembolso() {
     }
     
+    
     public DCBindingContainer getDCBindingsContainer(){
-        DCBindingContainer bindingsContainer = 
-    (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
+        DCBindingContainer bindingsContainer =   (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
         
         return bindingsContainer;
     }
 
-    public void calcularReembolsoGeneral(ActionEvent actionEvent) {
+    public String calcularReembolsoGeneral(ActionEvent actionEvent) {
         DCBindingContainer bindings = this.getDCBindingsContainer();
         DCIteratorBinding itorBinding = bindings.findIteratorBinding("PasajerosPorPNR1Iterator");
         ViewObject vo = itorBinding.getViewObject();
@@ -31,5 +33,31 @@ public class Reembolso {
             System.out.println("Ticket:" + row.getAttribute("TktEmitido") + " Costo: " + row.getAttribute("Costo"));             
         }
         System.out.println("Costo total:" + suma); 
+        
+        //parametroReembolso="Costo total:" + suma;
+        return "Costo total:" + suma;
+    }
+    
+    
+    public void setparametroReembolso(String parametroReembolso) {
+        this.parametroReembolso = parametroReembolso;
+    }
+
+
+    public String getParametroReembolso() {
+        DCBindingContainer bindings = this.getDCBindingsContainer();
+        DCIteratorBinding itorBinding = bindings.findIteratorBinding("PasajerosPorPNR1Iterator");
+        ViewObject vo = itorBinding.getViewObject();
+        Row[] selectRolesRows = vo.getFilteredRows("Seleccionar", true);
+        
+        int suma = 0;
+        System.out.println("Tickets seleccionados: " +selectRolesRows.length);
+        for(Row row : selectRolesRows){
+            suma = suma +  Integer.parseInt(row.getAttribute("Costo").toString());
+            System.out.println("Ticket:" + row.getAttribute("TktEmitido") + " Costo: " + row.getAttribute("Costo"));             
+        }
+        System.out.println("Costo total:" + suma); 
+        parametroReembolso = "Costo total:" + suma;
+        return parametroReembolso;
     }
 }
